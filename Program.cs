fb95170b05c0cdb608206b9ad1211bdb3b3d02c7
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 12:17:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static int LerLinhas()
+    {
+        int linhas;
+        Console.Write("Digite o número de linhas: ");
+        linhas = Convert.ToInt32(Console.ReadLine());
+        return linhas;
+    }
+
+    public static void DesenharTriangulo(int linhas)
+    {
+        int linha, coluna;
+
+        if (linhas > 10)
+        {
+            Console.WriteLine("O número de linhas não deve ser superior a 10.");
+            return; // termina já aqui a função DesenharTriangulo() e volta para a Main()
+        }
+
+        for (linha = 0; linha < linhas; linha++)
+        {
+            for (coluna = 0; coluna <= linha; coluna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // mudar para a linha seguinte
+        }
+
+    }
+
+    public static void Main(string[] args)
+    {
+        int linhas;
+
+        linhas = LerLinhas();
+        DesenharTriangulo(linhas);
+
+    }
+
+}
